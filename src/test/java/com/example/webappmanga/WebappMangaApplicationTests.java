195fbd187564a1f2f004d43dc0c721aa65e9f154
Kaisamira <thienthan726@gmail.com>
2024-01-02 18:04:30 +0700

    Push project

---
+++ src/test/java/com/example/webappmanga/WebappMangaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webappmanga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappMangaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
