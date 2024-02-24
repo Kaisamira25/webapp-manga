91e479009737c45299a4a939d7a03556f0f1b144
Do Phu <dothanhphu01012002@gmail.com>
2024-02-24 21:30:37 +0700

    #10 feat: testCallAPI

--- src/main/java/com/example/webappmanga/controller/Publications/GetPublicationsController.java
+++ src/main/java/com/example/webappmanga/controller/Publications/GetPublicationsController.java
@@ -9,13 +9,11 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/v1/publications")
 @RequiredArgsConstructor
@@ -23,6 +21,7 @@ public class GetPublicationsController {
 
     private final IPublications iPublications;
 
+//    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/all")
     @Operation(summary = "Find All Publications",responses = {
             @ApiResponse(description = "success", responseCode = "200")})
